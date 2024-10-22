f66d6bc2793577deacd825b8ea0564273da30bfd
KSH7-7 <ssafy12ksh@gmail.com>
2024-10-22 10:45:31 +0900

    README 작성 & 개인정보 삭제

--- Web_API/APIapp.js
+++ Web_API/APIapp.js
@@ -1,7 +1,7 @@
 const fetchGameStats = async () => {
     try {
-        const apiUrl = 'https://open.api.nexon.com/maplestory/v1/character/basic?ocid=a31eb419699425dcc4cc4ea6f09414e4efe8d04e6d233bd35cf2fabdeb93fb0d'; // 여기에 실제 게임 API URL을 입력하세요.
-        const apiKey = 'test_c9cbd0b4e288c4a4baf33f79d24626cb5435076d09e380d66c602405d4841a7eefe8d04e6d233bd35cf2fabdeb93fb0d';
+        const apiUrl = 'https://open.api.nexon.com/maplestory/v1/character/basic?ocid=???????????'; // 여기에 실제 게임 API URL을 입력하세요.
+        const apiKey = '???????????????????????';
         const response = await fetch(apiUrl);
         const data = await response.json();
 
@@ -25,5 +25,5 @@ const fetchGameStats = async () => {
 // 페이지가 로드될 때 데이터를 가져옴
 fetchGameStats();
 
-// 주기적으로 데이터를 갱신하고 싶다면, 예를 들어 5분마다 갱신
-setInterval(fetchGameStats, 300000); // 300000 밀리초 = 5분
+// 주기적으로 데이터를 갱신
+setInterval(fetchGameStats, 300000); 
